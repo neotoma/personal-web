d4502da5b7967829ca768f60536b01e30b522e38
Mark Hendrickson <markmhendrickson@gmail.com>
2016-07-29 14:48:06 +0200

    Fix app nav transitions

--- app/components/app-nav.js
+++ app/components/app-nav.js
@@ -1,12 +1,14 @@
 import Ember from 'ember';
 import ScrollingMixin from '../mixins/scrolling';
+import ComponentTransitionsMixin from '../mixins/component-transitions';
 
-export default Ember.Component.extend(ScrollingMixin, Ember.Evented, {
+export default Ember.Component.extend(ScrollingMixin, ComponentTransitionsMixin, Ember.Evented, {
   tagName: 'nav',
   classNames: ['app'],
-  classNameBindings: ['scrolled', 'loaded'],
+  classNameBindings: ['scrolled', 'shown'],
   appNav: Ember.inject.service(),
   store: Ember.inject.service(),
+  hidden: true,
 
   init() {
     this._super(...arguments);
@@ -14,7 +16,7 @@ export default Ember.Component.extend(ScrollingMixin, Ember.Evented, {
     var self = this;
 
     this.get('appNav').on('show', function() {
-      self.set('loaded', true);
+      self.set('hidden', false);
     });
 
     this.get('store').findAll('attribute').then(function(attributes) {

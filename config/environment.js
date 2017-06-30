5656ed859b16c8f21c4951e4c87ea5cf94c11475
Mark Hendrickson <markmhendrickson@gmail.com>
2017-06-30 12:29:45 +0200

    Improve package.json slightly
    And remove comments from environment file

--- config/environment.js
+++ config/environment.js
@@ -7,35 +7,24 @@ module.exports = function(environment) {
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
-      FEATURES: {
-        // Here you can enable experimental features on an ember canary build
-        // e.g. 'with-controller': true
-      }
+      FEATURES: {}
     },
 
     APP: {},
 
     intl: {
-      baseLocale: 'en-us' // default build-time locale 
+      baseLocale: 'en-us'
     }
   };
 
   if (environment === 'development') {
-    // ENV.APP.LOG_RESOLVER = true;
-    // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
     ENV.EmberENV.API_HOST = process.env.WEB_DEV_API_HOST;
     ENV.EmberENV.GA_TRACKING_ID = process.env.WEB_DEV_GA_TRACKING_ID;
   }
 
   if (environment === 'test') {
-    // Testem prefers this...
     ENV.baseURL = '/';
     ENV.locationType = 'none';
-
-    // keep test console output quieter
     ENV.APP.LOG_ACTIVE_GENERATION = false;
     ENV.APP.LOG_VIEW_LOOKUPS = false;
 

24070c28fa4a7dc49f8b44bb2fd3c7f22c119bad
Mark Hendrickson <markmhendrickson@gmail.com>
2018-01-29 16:55:11 +0100

    Strip markdown from photoAlbum route description

--- app/routes/photo-album.js
+++ app/routes/photo-album.js
@@ -1,5 +1,6 @@
 import attribute from 'personal-web/utils/attribute';
 import Ember from 'ember';
+import removeMarkdown from 'npm:remove-markdown';
 
 export default Ember.Route.extend({
   headData: Ember.inject.service(),
@@ -15,8 +16,10 @@ export default Ember.Route.extend({
     var photoAlbum = models['photoAlbum'];
     controller.set('photoAlbum', photoAlbum);
 
+    console.log(removeMarkdown(photoAlbum.get('description')));
+
     this.set('headData.canonicalUrl', attribute(models['attributes'], 'url') + this.router.generate('photo-album', photoAlbum.get('id')));
-    this.set('headData.description', photoAlbum.get('description'));
+    this.set('headData.description', removeMarkdown(photoAlbum.get('description')));
     this.set('headData.imageUrl', photoAlbum.get('coverPhoto.largeImageUrl'));
     this.set('headData.title', photoAlbum.get('title'));
   },

7f6c91a1747c0fffec22bc4bd7d2d2713e146954
Mark Hendrickson <markmhendrickson@gmail.com>
2018-04-10 16:20:30 +0200

    Improve books route and styles

--- app/routes/books.js
+++ app/routes/books.js
@@ -1,4 +1,9 @@
-import Route from '@ember/routing/route';
+import Ember from 'ember';
 
-export default Route.extend({
+export default Ember.Route.extend({
+  headData: Ember.inject.service(),
+
+  setupController() {
+    this.set('headData.title', 'Books');
+  }
 });

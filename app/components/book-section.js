903275be11b39eebe5efe5411e180a571deaea41
Mark Hendrickson <markmhendrickson@gmail.com>
2018-07-05 19:40:18 +0200

    Add publisher and purchase URL support for books

--- app/components/book-section.js
+++ app/components/book-section.js
@@ -2,5 +2,9 @@ import Component from '@ember/component';
 
 export default Component.extend({
   classNames: ['book'],
-  tagName: 'section'
+  tagName: 'section',
+
+  purchaseButtonLabel: Ember.computed('book.releasedAt', function() {
+    return (this.get('book.pendingRelease')) ? 'Pre-Order Now' : 'Order Now';
+  }),
 });
